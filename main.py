b34b58e3131b695262029b69bbcc4992947ee02c
kimmieharding <kah46@njit.edu>
2025-12-02 18:31:07 -0500

    Annotated Images of Skeleton using MediaPipe

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import glob
 import os
 import cv2 as cv
 import mediapipe as mp
+from mediapipe import solutions
+from mediapipe.framework.formats import landmark_pb2
 import numpy as np
 from ultralytics import YOLO
 
@@ -41,10 +43,11 @@ def faceDetection(img):
 
 def skeletonDetection(img):
     BaseOptions = mp.tasks.BaseOptions
-    PoseLandmarker = mp.tasks.vision.PoseLandmarker
-    PoseLandmarkerOptions = mp.tasks.vision.PoseLandmarkerOptions
-    VisionRunningMode = mp.tasks.vision.RunningMode
+    PoseLandmarker = mp.tasks.vision.PoseLandmarker #class to detect human pose landmarks
+    PoseLandmarkerOptions = mp.tasks.vision.PoseLandmarkerOptions #class for setting pose detection options
+    VisionRunningMode = mp.tasks.vision.RunningMode #Set running mode: IMAGE, VIDEO, or LIVE_STREAM
 
+    #assign the trained model for pose detection
     options = PoseLandmarkerOptions(
         base_options=BaseOptions(model_asset_path=skeleton_model),
         running_mode=VisionRunningMode.IMAGE)
@@ -53,6 +56,22 @@ def skeletonDetection(img):
         mp_image = mp.Image.create_from_file(img)
         pose_landmarker_result = landmarker.detect(mp_image)
     
+    # Loop through the detected poses to annotate image.
+    annotated_image = np.copy(mp_image.numpy_view())
+
+    for pose_landmarks in pose_landmarker_result.pose_landmarks:
+        # Draw the pose landmarks.
+        pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+        pose_landmarks_proto.landmark.extend([landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in pose_landmarks])
+        solutions.drawing_utils.draw_landmarks(
+        annotated_image,
+        pose_landmarks_proto,
+        solutions.pose.POSE_CONNECTIONS,
+        solutions.drawing_styles.get_default_pose_landmarks_style())
+
+    cv.imshow("Skeleton Landmarks Image", annotated_image)
+    cv.waitKey(0)
+
     return pose_landmarker_result
 
 if __name__ == '__main__':
